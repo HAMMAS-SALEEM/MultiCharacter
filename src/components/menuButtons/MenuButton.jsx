870cf75a29b89a2d2d22ff9a7a7fb76ccd39a409
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-06-21 16:32:20 +0500

    Add minor changes

--- src/components/menuButtons/MenuButton.jsx
+++ src/components/menuButtons/MenuButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MenuButton = ({ button }) => {
   return (
-    <button type='button' className='text-white font-bold text-2xl'>
+    <button type='button' className='text-white font-bold text-2xl self-end'>
       {button.button}
     </button>
   )
